5c391018cd1bf219fcaea5d1f74319feb95046d5
jigkoxsee <ked.naja@gmail.com>
2018-10-19 00:39:04 +0700

    Add ns to param, add !log command

--- src/kubee/core.cljs
+++ src/kubee/core.cljs
@@ -12,27 +12,62 @@
     (.send msg.channel reply-msg)
     (prn :not-reply-bot)))
 
+(defn reply-ln [msg text]
+  (let [lines  (clojure.string/split-lines text)]
+     (doseq [p (partition 10 10 "" lines)]
+        (prn :seq (count p))
+        (reply msg (clojure.string/join "\n" p)))))
+
+(defn get-pod [msg [k-ns]]
+  (prn :ns k-ns)
+  (sh.exec
+   (str "kubectl get pod -n " k-ns)
+   (fn [code out err]
+     (when out
+      (reply-ln msg out))
+     (when (not= code 0)
+      (reply-ln msg (str ":boom: get pod error: " err)))))) 
+        
+
+(defn get-log [msg [k-ns k-pod k-tail]]
+  (when (some nil? [k-ns k-pod k-tail])
+    (reply msg "not enough param"))
+  (let [log-tail (min 50 k-tail)]
+    (prn :ns k-ns :pod k-pod :tail log-tail)
+    (sh.exec 
+     (str "kubectl logs -n " k-ns " --tail=" log-tail " " k-pod)
+     (fn [code out err]
+       (when out
+        (reply-ln msg out))
+       (when (not= code 0)
+        (reply-ln msg (str ":boom: get log error: " err)))))))    
+        
+
 (.on client "ready" #(prn :connected-and-ready))
 
 (.on client "message"
      (fn [msg]
+       (let [remove-space (filter #(not= " " %))
+             params (-> msg.content
+                        (clojure.string/split " "))
+             filtered (filter #(not (clojure.string/blank? %)) params)
+             cmd    (first params)]
+         
+         (prn :cmd cmd)
+         (prn :params filtered)
 
-       (when (= msg.content "!!ping")
-         (reply msg "!ping"))
-
-       (when (= msg.content "!ping")
-         (reply msg "Pong"))
-
-       (when (= msg.content "pod")
-         (sh.exec
-           "kubectl get pod"
-           (fn [code out err]
-             (let [lines  (clojure.string/split-lines out)]
-               (doseq [p (partition 10 10 "" lines)]
-                 (prn :seq (count p))
-                 (reply msg (clojure.string/join "\n" p)))))))
-
-       (prn :msg (js/Object.keys msg))))
+         (when (= cmd "!!ping")
+           (reply msg "!ping"))
+     
+         (when (= cmd "!ping")
+           (reply msg "Pong"))
+         
+         (when (= cmd "!log")
+           (get-log msg (rest filtered)))  
+     
+         (when (= cmd "!pod")
+           (get-pod msg (rest filtered))))))  
+           
 
 (defn -main [args] 
   (js/console.log "hello" args)
